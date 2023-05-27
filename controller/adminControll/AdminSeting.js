c6dd27aa1391a3651fa7fe403b130e4ad6503d83
bomzaza <bombanlang2539@gmail.com>
2023-05-28 04:23:16 +0700

    28-5

--- controller/adminControll/AdminSeting.js
+++ controller/adminControll/AdminSeting.js
@@ -1,12 +1,9 @@
-
-const room = require("../../models/room")
 const connectionn = require('../../server')
-
-module.exports =   (req, res) => {
-  connectionn.query(`SELECT * FROM ROOM`,
-       (err, results) => {
-        //res.json(results)
-        res.render('allSeting', {results})
-        }
-      ) 
+module.exports = (req, res) => {
+     connectionn.query(`SELECT * FROM ROOM`,
+     (err, results) => {
+      //res.json(results)
+      res.render('allSeting', {results})
+      }
+    ) 
 }
